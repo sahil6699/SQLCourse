fedaae385c95d0240f8bd92600fc4ba33f5f5691
sahil aneja <55090396+sahil6699@users.noreply.github.com>
2025-05-24 18:38:54 +0530

    where clause lecture

--- MySqlServer/Basics Part 2/Adventure works.sql
+++ MySqlServer/Basics Part 2/Adventure works.sql
@@ -44,11 +44,142 @@ SELECT top 2 [ShiftID],
 [ModifiedDate] from HumanResources.Shift;
 Go
 -- Employee table
-SELECT TOP 5 * FROM HumanResources.Employee
+SELECT TOP 5 [BusinessEntityID],
+[NationalIDNumber],
+[LoginID],
+[OrganizationNode],
+[OrganizationLevel],
+[JobTitle],
+[BirthDate],
+[MaritalStatus],
+[Gender],
+[HireDate],
+[SalariedFlag],
+[VacationHours],
+[SickLeaveHours],
+[CurrentFlag],
+[rowguid],
+[ModifiedDate] FROM HumanResources.Employee
 Go
 
 -- Employee pay history table
-SELECT top 4 * from HumanResources.EmployeePayHistory
+SELECT top 4 [BusinessEntityID],
+    [RateChangeDate],
+    [Rate],
+    [PayFrequency],
+    [ModifiedDate]
+ from HumanResources.EmployeePayHistory
 
 -- JobCandidate table
-SELECT top 6 * from HumanResources.JobCandidate
+SELECT top 6 [JobCandidateID],
+    [BusinessEntityID],
+    [Resume],
+    [ModifiedDate] from HumanResources.JobCandidate
+
+-------------------------------
+-- WHERE CLAUSE
+-- where clause is used to filter data
+SELECT [DepartmentID],
+    [Name],
+    [GroupName],
+    [ModifiedDate]
+ FROM HumanResources.Department
+--  WHERE DepartmentID > 7 
+-- WHERE Name='Production Control';
+-- WHERE GroupName='MANUFACTURING'
+---- this gives result that ends with production
+-- WHERE [Name]  LIKE '%Production'
+-- this gives results that starts with Production
+-- this search is generally more faster as sql normally finds thing by first sorting them in alphabetical order and then finding them
+-- WHERE [Name] LIKE 'Production%';
+-- when we to find something i.e in between the word(string) then we use %automation1%
+-- WHERE [Name] LIKE '%Production%'
+-- WHERE GroupName = 'Manufacturing' -- is equal
+-- WHERE GroupName <> 'Manufacturing' -- is not equal
+WHERE DepartmentID <> 7 -- this gives results with departmentId not equal to 7
+
+
+-- we can't compare with NULL using =(equal to) and <> (not equal to) it will always give false result as comparison with null using these operators is not possible
+-- eg:- 
+-- this is false because not we can't compare to null not becuase there is no null value in table
+SELECT * from HumanResources.Department
+where GroupName = NULL
+
+-- similarty not equal to null also not give any ouput as we can't compare with null
+SELECT * from HumanResources.Department
+WHERE GroupName <> NULL
+
+-- we can also compare null = null this will also give empty ouput
+-- this should have true according to a newbie programmer but since in sql we can't compare to null hence it will be false
+SELECT * from HumanResources.Department
+WHERE null = null
+
+-- similarly we null <> null is also gives false for condtion or no ouput
+SELECT * from HumanResources.Department
+where NULL <> NULL
+
+-- For comparing with NULL we have to use 
+-- IS NULL -- for checking values equal to NULL
+SELECT [DepartmentID],
+[Name],
+[GroupName],
+[ModifiedDate] from HumanResources.Department
+where GroupName IS NULL
+
+
+-- IS NOT NULL -- for checking values which are not equal to null
+SELECT [DepartmentID],
+    [Name],
+    [GroupName],
+    [ModifiedDate]
+ FROM HumanResources.Department
+ WHERE GroupName IS NOT NULL
+
+--  if we wanted to  print multiple values then we could 'IN' keyword
+SELECT [DepartmentID],
+    [Name],
+    [GroupName],
+    [ModifiedDate]
+FROM HumanResources.Department
+-- finding multiple integer values
+-- WHERE DepartmentID IN(7,11) -- this will give results which have DepartmentId as 7 and 11
+-- finding multiple string values
+-- WHERE [Name] in ('PRODUCTION','Sales') -- this will gives resuls which have name as Sales and Production
+-- for finding something in between INT values it doesn't work for string and both starting no and end no are included in result
+WHERE DepartmentID between 7 AND 11
+
+
+-- using AND and Or keywords
+
+-- for AND both condition need to be true
+SELECT * 
+from HumanResources.Department
+where GroupName = 'Manufacturing'
+AND DepartmentID BETWEEN 7  AND 11
+
+-- for OR even if one condition is true the results come
+-- its almost like finding results for two different condition and then joining them
+SELECT * FROM 
+HumanResources.Department
+WHERE GroupName = 'Manufacturing'
+or DepartmentID BETWEEN 7 and 11
+
+
+-- assignment find all the employees which are married
+SELECT [BusinessEntityID],
+[NationalIDNumber],
+[LoginID],
+[OrganizationNode],
+[OrganizationLevel],
+[JobTitle],
+[BirthDate],
+[MaritalStatus],
+[Gender],
+[HireDate],
+[SalariedFlag],
+[VacationHours],
+[SickLeaveHours],
+[CurrentFlag],
+[rowguid],
+[ModifiedDate] from HumanResources.Employee
+WHERE MaritalStatus = 'M'
